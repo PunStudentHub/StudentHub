0f663966c759015592e85887c84951e4105b138b
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-07-05 08:52:38 -1000

    Update

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,19 @@
 module ApplicationHelper
   def get_title page_name
-    
-    
+    default_title = "Punahou Student Hub"
+    if page_name.empty?
+      default_title
+    else
+      page_name + " | " + default_title
+    end
+
   end  
+
+  def navbar_current?(button, page_title = '')
+    if button == page_title
+      'nav-item nav-link active' 
+    else
+      'nav-item nav-link'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get '/help', to: 'pages#help'
+  get '/contact', to: 'pages#contact'
+  get '/home', to: 'pages#home'
   get 'pages/home'
-  get 'pages/help'
-  get 'pages/contact'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
