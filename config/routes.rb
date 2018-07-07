39d30ed7542338415685e4533a3fa234959b4a90
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-07-07 12:34:13 -1000

    Add footer and fix CSS stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   get '/help', to: 'pages#help'
   get '/contact', to: 'pages#contact'
   get '/home', to: 'pages#home'
+  get '/signup', to: 'users#new'
   get 'pages/home'
   root 'pages#home'
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
