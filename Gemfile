07feda3a99763d36620384270ae5a2db12e7b283
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-08-18 12:14:37 -1000

    Play around with homepage layout

--- Gemfile
+++ Gemfile
@@ -49,7 +49,7 @@ group :development do
 end
 
 group :production do
-  gem 'pg', '0.20.0'
+  gem 'pg', '1.0'
 end
 
 group :test do
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -1,6 +1,7 @@
 class Announcement < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, dependent: destroy
   validates :user_id, presence: true
+  default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: {maximum: 10000}
   validates :title, presence: true, length: {maximum: 140}
 
