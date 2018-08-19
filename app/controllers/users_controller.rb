bb7d35d310d951d86971a56d5c1515da76c8bfb7
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-08-19 11:21:48 -1000

    Clean up announcement displays and login system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: [:destroy]
+  before_action :not_logged_in, only: [:new, :create]
 
 
   def new
@@ -53,11 +54,18 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
+
+    def not_logged_in
+      if logged_in?
+        redirect_to(root_url) 
+        flash[:danger] = "You aren't allowed to do that!"
+      end
+    end
+
 
 end
 
