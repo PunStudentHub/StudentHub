46c52b548222ec5231f86f10822e3e37823a72ff
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-08-18 19:41:42 -1000

    Finish display properties of announcements

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,2 +1,9 @@
 class AnnouncementsController < ApplicationController
+  def show
+    @announcement = Announcement.find(params[:id])
+  end
+
+  def index
+    @announcements = Announcement.all.paginate(page: params[:page])
+  end
 end
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -4,5 +4,4 @@ class Announcement < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   validates :content, presence: true, length: {maximum: 10000}
   validates :title, presence: true, length: {maximum: 140}
-
 end
