ad5479a33a62c6ad0afcd05f61f19a93ee5c8476
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-08-24 11:43:39 -1000

    fix stuff maybe?

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,10 +1,7 @@
 module AnnouncementsHelper
   def filter_announcements_class_years
-    class_year_ids = session[:class_year_ids].map{|s| s.to_i}
     Announcement.joins(:class_years)
                 .distinct
-                .where(class_years: { id: class_year_ids})
-
-
+                .where(class_years: { id: get_class_year_ids})
   end
 end
