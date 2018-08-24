b6c5045517112696315aebcfe699b98219bf04b8
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-08-23 23:30:24 -1000

    crisis averted

--- app/helpers/announcements_helper.rb
+++ app/helpers/announcements_helper.rb
@@ -1,6 +1,10 @@
 module AnnouncementsHelper
   def filter_announcements_class_years
     class_year_ids = session[:class_year_ids].map{|s| s.to_i}
-    Announcement.joins(:class_years).where(class_years: { id: class_year_ids})
+    Announcement.joins(:class_years)
+                .distinct
+                .where(class_years: { id: class_year_ids})
+
+
   end
 end
--- app/models/announcement.rb
+++ app/models/announcement.rb
@@ -2,7 +2,7 @@ class Announcement < ApplicationRecord
   belongs_to :user, dependent: :destroy
   has_and_belongs_to_many :class_years, dependent: :destroy
   validates :user_id, presence: true
-  default_scope -> { order(created_at: :desc) }
+  default_scope -> { order(updated_at: :desc) }
   validates :content, presence: true, length: {maximum: 10000}
   validates :title, presence: true, length: {maximum: 140}
 
