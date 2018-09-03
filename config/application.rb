e5dcc8ef6be5308689d03a0852ed35b8d34056d0
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-09-03 10:27:14 -1000

    Remove unnecessary autoloading stuff

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ module StudentHub
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.enable_dependency_loading = true
-    config.eager_load_paths << Rails.root.join('app/models/concerns')
+    #config.enable_dependency_loading = true
+    #config.eager_load_paths << Rails.root.join('app/models/concerns')
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
