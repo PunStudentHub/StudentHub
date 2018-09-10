e4b2740571addd073edd7b512eb7ea12ebee87c4
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-09-09 21:09:28 -1000

    Add ability to import CSV, and also a CSV to import

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,7 @@
 class Subject < ApplicationRecord
   include Friendlyable
   has_and_belongs_to_many :timeslots, dependent: :destroy
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  default_scope { order(:name)}
 
 end
