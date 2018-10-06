5043d178d21d000042399bb8c4970a4bf7bf7606
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-10-05 21:14:12 -1000

    Does this fix the lag on production?

--- app/controllers/plc_controller.rb
+++ app/controllers/plc_controller.rb
@@ -2,12 +2,15 @@ require 'json'
 class PlcController < ApplicationController
   def index
     @subjects = Subject.all
-    if params[:subject]
-      @selected_subject = @subjects.find_by_hash_id(params[:subject])
-      @timeslots = @selected_subject.timeslots
-    else
-      @selected_subject = nil
-      @timeslots = nil
+    @array = []
+    Subject.all.each do |subject|
+      @array << {
+        name: subject.name,
+        hash_id: subject.hash_id,
+        timeslots: subject.timeslots.map do |t|
+          [t.day, t.time]
+        end
+      }
     end
   end
 
