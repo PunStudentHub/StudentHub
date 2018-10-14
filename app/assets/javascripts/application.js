4878ae1e857bef4c73046fd58cd1b930d9949442
Devin Hartzell <dchotzen-hartzell19@punahou.edu>
2018-10-14 10:47:09 -1000

    ??git add .

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -70,10 +70,4 @@ function update(model, id) {
   })
 }
 
-document.addEventListener('turbolinks:load', event => {
-  if (typeof ga === 'function') {
-    ga('set', 'location', event.data.url);
-    ga('send', 'pageview');
-  }
-});
 
