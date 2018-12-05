faec2268334fee19653ef1291a3b6ae23223bae8
cbell19 <bgtybgtybgty1@gmail.com>
2018-12-04 18:34:03 -1000

    Revamped the home page evnts section closed #28

--- app/models/event.rb
+++ app/models/event.rb
@@ -15,12 +15,14 @@ class Event < ApplicationRecord
   validate :date_in_future
   has_and_belongs_to_many :users
 
+  scope :finalized, -> { where(final: true) }
   scope :today, -> { where(start_time: DateTime.now.getlocal('-10:00').beginning_of_day..DateTime.now.getlocal('-10:00').end_of_day) }
   scope :tomorrow, -> { where(start_time: (DateTime.now.getlocal('-10:00').beginning_of_day + 1.day)..(DateTime.now.getlocal('-10:00').end_of_day + 1.day)) }
   scope :day_after_tomorrow, -> { where(start_time: (DateTime.now.getlocal('-10:00').beginning_of_day + 2.days)..(DateTime.now.getlocal('-10:00').end_of_day + 2.days)) }
   scope :future_events, -> { where("start_time > ?", DateTime.now.getlocal('-10:00')) }
   scope :past_events, -> { where("start_time < ?", DateTime.now.getlocal('-10:00')) }
-
+  scope :future_day_events, -> { where("start_time > ?", DateTime.now.getlocal('-10:00').beginning_of_day) }
+  scope :set_day_events, -> (day) { where(start_time: day.beginning_of_day..day.end_of_day)}
 
 
   def ends_after_start
